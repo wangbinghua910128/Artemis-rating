ls1intum/Artemis

Implemtation, Testing, Documentation and Configuration

src/main/java/de/tum/cit/aet/artemis/core/service/ProfileService.java
@@ -130,4 +130,22 @@ public class ProfileService {
     public boolean isProductionActive() {
         return isProfileActive(JHipsterConstants.SPRING_PROFILE_PRODUCTION);
     }
+
+    /**
+     * Checks if the docker profile is active
+     *
+     * @return true if the docker profile is active, false otherwise
+     */
+    public boolean isDockerActive() {
+        return isProfileActive(Constants.PROFILE_DOCKER);
+    }
+
+    /**
+     * Checks if the sharing profile is active
+     *
+     * @return true if the sharing profile is active, false otherwise
+     */
+    public boolean isSharing() {
+        return isProfileActive(Constants.PROFILE_SHARING);
+    }
 }

wangbinghua910128/Artemis-rating

src/main/java/de/tum/cit/aet/artemis/core/service/ProfileService.java
@@ -132,4 +132,22 @@ public class ProfileService {
     public boolean isProductionActive() {
         return isProfileActive(JHipsterConstants.SPRING_PROFILE_PRODUCTION);
     }
+
+    /**
+     * Checks if the docker profile is active
+     *
+     * @return true if the docker profile is active, false otherwise
+     */
+    public boolean isDockerActive() {
+        return isProfileActive(Constants.PROFILE_DOCKER);
+    }
+
+    /**
+     * Checks if the sharing profile is active
+     *
+     * @return true if the sharing profile is active, false otherwise
+     */
+    public boolean isSharingActive() {
+        return isProfileActive(Constants.PROFILE_SHARING);
+    }
 }